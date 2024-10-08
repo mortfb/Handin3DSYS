916cb60e57045da575b6f2feeec9d5c424475c13
Morten <mortbonneland@gmail.com>
2024-10-08 13:35:46 +0200

    proto and early server

---
+++ server/server.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	proto "handin3/grpc"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type ChittyChatServiceServer struct {
+	proto.UnimplementedChittyChatServiceServer
+	users []string //maybe not needed
+}
+
+func main() {
+	server := &ChittyChatServiceServer{users: []string{}}
+
+	server.start_server()
+
+}
+
+func (s *ChittyChatServiceServer) start_server() {
+	grpcServer := grpc.NewServer()
+	listener, err := net.Listen("tcp", ":5050")
+	if err != nil {
+		log.Fatalf("Did not work")
+	}
+
+	//Register the server, from proto-file
+	proto.RegisterChittyChatServiceServer(grpcServer, s)
+
+	err = grpcServer.Serve(listener)
+
+	if err != nil {
+		log.Fatalf("Did not work")
+	}
+}
